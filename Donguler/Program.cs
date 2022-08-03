5132c2691cd8fbe08a716192bb4f29be0bd3ad49
Gokhan <Gokhan@DESKTOP-G6HREUR>
2022-08-03 19:15:06 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Donguler 
+            //Birbirine benzeyen işlemleri tekrar etmek için kullanırız
+
+            //i=i+2 || i+=2 ikişer ikişer artırma
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Dongu Bitti \n");
+
+            string kurs1 = "Yazilim Gelistirici Kampi C#";
+            string kurs2 = "Programlamaya baslangic icin temel kursu";
+            string kurs3 = "Yazilim Gelistirici Kampi Java";
+            string kurs4 = "Python";
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+
+            //Array -dizi 
+            string[] kurslar = new string[] { "Yazilim Gelistirici Kampi C#", "Programlamaya baslangic icin temel kursu", "Yazilim Gelistirici Kampi Java","C++","Python"};
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For dongusu bitti");
+
+            //Dizi temelli yapıları tek tek dönmeye yarıyor foreach
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - footer");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace KampIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Değişkenler Veri Tutucudur
+            //Değişkenler aslında programlamanin en temel konusudur ve biz veriyi onunla yönetiriz.
+
+            //Verileri türlerine göre ayırıyoruz
+            //String - Metinsel ifadeleri tutmamızı sağlar
+
+            //Type safety - tip güvenliği  - hangi veri türünü girersen çıktısı da o veri türü olmak zorunda 
+            string kategoriEtiketi = "Kategori";
+            Console.WriteLine(kategoriEtiketi);
+
+            //Don't Repeat Yourself! - Kendini asla tekrar etme
+            //Console.WriteLine("Kategori"); !! Hatalı projede bir yeri değiştirmek zorunda kalırsak tek bir noktadan değiştiremiyoruz 
+            //KategoriEtiketi burada değer tutucu yani takma isim alias
+
+            //İnt - Tam sayı veri tiplerini tutar
+            int ogrenciSayisi = 32000;
+
+            //Double - Decimal Ondalıklı sayıları tutar.
+            double faizOrani = 1.45;
+
+            //Bool - Mantıksal veri tipidir True or False değerini alır.
+            bool sistemeGirisYapilmisMi=true;
+
+            //Şart Blokları
+
+            if (sistemeGirisYapilmisMi == true)
+            {
+                Console.WriteLine("Sisteme giriş başarılı");
+            }
+            else
+            {
+                Console.WriteLine("Giriş başarısız");
+            }
+
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Oku");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış oku");
+
+            }
+            else
+            {
+                Console.WriteLine("Eşittir oku");
+            }
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
