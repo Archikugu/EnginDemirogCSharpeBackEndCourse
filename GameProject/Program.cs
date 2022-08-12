5180502352d24af7deb45ff8e6c4738a7810f61a
Gokhan <engokhangok@gmail.com>
2022-08-12 18:00:08 +0300

    Yazılım Geliştirici Yetiştirme Kampı

---
+++ GameProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 1999, FirstName = "Gökhan", LastName = "Gök", IdentityNumber = 1234567890 });
+
+            GamerManager gamerManager1 = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager1.Add(new Gamer { Id = 1, BirthYear = 1999, FirstName = "Gökhan", LastName = "Gök", IdentityNumber = 1234567890 });
+        }
+    }
+}
